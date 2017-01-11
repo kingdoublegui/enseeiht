5a1db0584451df617e8f233dfecce051c7f2fbf9
Thibault Meunier <contact@thibaultmeunier.com>
2017-01-11 10:48:27 +0100

    "Changement sur le lagrangien"

--- 2IMA/OptimisationNumerique/lagrangien.m
+++ 2IMA/OptimisationNumerique/lagrangien.m
@@ -1,9 +1,12 @@
-function [ x, l, u ] = lagrangien( f, c, u0, t, nc0, a, b )
+function [ x, lambda, mu ] = lagrangien( f, x0, lambda0, c, u0, tau, etac0, alpha, beta )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-e0 = 1/u0;
-n0 = nc0/u0^a;
+x = x0;
+lambda = lambda0;
+epsilon0 = 1/u0;
+epsilon = epsilon0;
+eta = etac0/u0^alpha;
 
 nIter = 100;
 
@@ -11,10 +14,20 @@ k = 1;
 while k <= nIter
     
     
-    k = k+1;
     % Critere de convergence non satisfait
     if k <= nIter
-       
+       k = k+1;
+       if c(x_k1) <= eta_k
+           lambda = lambda + mu*c(x_k1);
+           mu = mu;
+           epsilon = epsilon/mu;
+           eta = eta/mu^beta;
+       else
+           lambda = lambda;
+           mu = tau*mu;
+           epsilon = epsilon0/mu;
+           eta = etac0/mu^beta;
+       end
     end
 end
 
