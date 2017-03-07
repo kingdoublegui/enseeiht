b04b401e3a63328bc3effd2afb763603089f7eef
Thibault Meunier <contact@thibaultmeunier.com>
2017-03-07 10:22:24 +0100

    [VHDL] Milieu tp3

---
+++ 2IMA/Architecture2/tp4/ipcore_dir/compteur.vhd
@@ -0,0 +1,98 @@
+--------------------------------------------------------------------------------
+--    This file is owned and controlled by Xilinx and must be used solely     --
+--    for design, simulation, implementation and creation of design files     --
+--    limited to Xilinx devices or technologies. Use with non-Xilinx          --
+--    devices or technologies is expressly prohibited and immediately         --
+--    terminates your license.                                                --
+--                                                                            --
+--    XILINX IS PROVIDING THIS DESIGN, CODE, OR INFORMATION "AS IS" SOLELY    --
+--    FOR USE IN DEVELOPING PROGRAMS AND SOLUTIONS FOR XILINX DEVICES.  BY    --
+--    PROVIDING THIS DESIGN, CODE, OR INFORMATION AS ONE POSSIBLE             --
+--    IMPLEMENTATION OF THIS FEATURE, APPLICATION OR STANDARD, XILINX IS      --
+--    MAKING NO REPRESENTATION THAT THIS IMPLEMENTATION IS FREE FROM ANY      --
+--    CLAIMS OF INFRINGEMENT, AND YOU ARE RESPONSIBLE FOR OBTAINING ANY       --
+--    RIGHTS YOU MAY REQUIRE FOR YOUR IMPLEMENTATION.  XILINX EXPRESSLY       --
+--    DISCLAIMS ANY WARRANTY WHATSOEVER WITH RESPECT TO THE ADEQUACY OF THE   --
+--    IMPLEMENTATION, INCLUDING BUT NOT LIMITED TO ANY WARRANTIES OR          --
+--    REPRESENTATIONS THAT THIS IMPLEMENTATION IS FREE FROM CLAIMS OF         --
+--    INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A   --
+--    PARTICULAR PURPOSE.                                                     --
+--                                                                            --
+--    Xilinx products are not intended for use in life support appliances,    --
+--    devices, or systems.  Use in such applications are expressly            --
+--    prohibited.                                                             --
+--                                                                            --
+--    (c) Copyright 1995-2017 Xilinx, Inc.                                    --
+--    All rights reserved.                                                    --
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- You must compile the wrapper file compteur.vhd when simulating
+-- the core, compteur. When compiling the wrapper file, be sure to
+-- reference the XilinxCoreLib VHDL simulation library. For detailed
+-- instructions, please refer to the "CORE Generator Help".
+
+-- The synthesis directives "translate_off/translate_on" specified
+-- below are supported by Xilinx, Mentor Graphics and Synplicity
+-- synthesis tools. Ensure they are correct for your synthesis tool(s).
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+-- synthesis translate_off
+LIBRARY XilinxCoreLib;
+-- synthesis translate_on
+ENTITY compteur IS
+  PORT (
+    clk : IN STD_LOGIC;
+    sclr : IN STD_LOGIC;
+    q : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
+  );
+END compteur;
+
+ARCHITECTURE compteur_a OF compteur IS
+-- synthesis translate_off
+COMPONENT wrapped_compteur
+  PORT (
+    clk : IN STD_LOGIC;
+    sclr : IN STD_LOGIC;
+    q : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
+  );
+END COMPONENT;
+
+-- Configuration specification
+  FOR ALL : wrapped_compteur USE ENTITY XilinxCoreLib.c_counter_binary_v11_0(behavioral)
+    GENERIC MAP (
+      c_ainit_val => "0",
+      c_ce_overrides_sync => 0,
+      c_count_by => "1",
+      c_count_mode => 0,
+      c_count_to => "1",
+      c_fb_latency => 0,
+      c_has_ce => 0,
+      c_has_load => 0,
+      c_has_sclr => 1,
+      c_has_sinit => 0,
+      c_has_sset => 0,
+      c_has_thresh0 => 0,
+      c_implementation => 0,
+      c_latency => 1,
+      c_load_low => 0,
+      c_restrict_count => 0,
+      c_sclr_overrides_sset => 1,
+      c_sinit_val => "0",
+      c_thresh0_value => "1",
+      c_verbosity => 0,
+      c_width => 2,
+      c_xdevicefamily => "artix7"
+    );
+-- synthesis translate_on
+BEGIN
+-- synthesis translate_off
+U0 : wrapped_compteur
+  PORT MAP (
+    clk => clk,
+    sclr => sclr,
+    q => q
+  );
+-- synthesis translate_on
+
+END compteur_a;
